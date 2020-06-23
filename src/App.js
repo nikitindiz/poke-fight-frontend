54ada1a374a4f22ea07a31639de7626d8c1b927e
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-06-23 12:52:42 +0300

    Page layout added

--- src/App.js
+++ src/App.js
@@ -1,58 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import PokemonListContainer from './containers/PokemonListContainer';
 
-// class Counter extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       counter: 0,
-//     }
-//   }
-
-//   render() {
-//   return (
-//     <button
-//       onClick={() => {
-//         this.setState({ counter: this.state.counter + 1 })
-//       }}
-//     >
-//       {this.state.counter}
-//     </button>
-//   )
-//   }
-// }
-
-// const Counter = (props) => {
-//   const initialState = 0;
-
-//   // const arrayOfTwoElements = useState(initialState);
-//   // const stateItself = arrayOfTwoElements[0];
-//   // const functionToUpdateTheState = arrayOfTwoElements[1];
-
-//   const [ stateItself, functionToUpdateTheState ] = useState(initialState);
-
-//   // Component did mount only
-//   useEffect(() => {
-//     console.log('hi');
-//   }, []); // Empty array - no values to watch
-
-//   return (
-//     <button
-//       onClick={() => {
-//         functionToUpdateTheState(stateItself + 1)
-//       }}
-//     >
-//       {stateItself}
-//     </button>
-//   )
-// }
+import PageLayout from './components/PageLayout';
 
 const App = () => {
   return (
     <div className="App">
-      <PokemonListContainer />
+      <PageLayout />
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PokemonListContainer from '../containers/PokemonListContainer';
+
+const Content = () => {
+    return (
+        <div className="app-content">
+            <PokemonListContainer />
+        </div>
+    );
+};
+
+export default Content;
