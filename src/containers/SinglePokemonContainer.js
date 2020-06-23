816b9f824f9351cb6e88b27316e72c35eda2a468
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-06-23 16:20:05 +0300

    Subrouting is done

--- src/containers/SinglePokemonContainer.js
+++ src/containers/SinglePokemonContainer.js
@@ -1,35 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import PokemonList from '../components/PokemonList';
+import { useParams } from 'react-router-dom';
+
+import SinglePokemon from '../components/SinglePokemon';
 
 // Smart - it handles the business logic
-const PokemonListContainer = () => {
+const SinglePokemonContainer = () => {
+    const { id } = useParams();
+
     // IDLE, STARTED, SUCCEED, FAILED
     const [ fetchStatus, updateFetchStatus ] = useState('IDLE');
     const [ fetchError, setFetchError ] = useState(null);
-    const [ pokemonList, setPokemonList ] = useState([]);
+    const [ singlePokemon, setSinglePokemon ] = useState({});
 
     useEffect(() => {
       updateFetchStatus('STARTED');
 
-      fetch('https://frozen-sands-71650.herokuapp.com/pokemon')
+      fetch(`https://frozen-sands-71650.herokuapp.com/pokemon/${id}`)
         .then(response => response.json())
         .then(data => {
-          setPokemonList(data);
+          setSinglePokemon(data);
           updateFetchStatus('SUCCEED');
         })
         .catch(err => { // new Error('message') -> err.message
           setFetchError(err.message);
           updateFetchStatus('FAILED');
         })
-    }, []);
+    }, [id]);
 
     return (
-      <PokemonList
+      <SinglePokemon
         fetchStatus={fetchStatus}
-        pokemonList={pokemonList}
         fetchError={fetchError}
+        singlePokemon={singlePokemon}
       />
-    )
+    );
   }
 
-  export default PokemonListContainer;
+  export default SinglePokemonContainer;
